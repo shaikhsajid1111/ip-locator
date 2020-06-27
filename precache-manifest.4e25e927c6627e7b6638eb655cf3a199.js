122465bd030085ce31e98cf3da0686ea75830451
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-06-27 15:33:53 +0530

    Updates

---
+++ precache-manifest.4e25e927c6627e7b6638eb655cf3a199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea58ca179c4ce6c2c4cd254f9c53847",
+    "url": "/ip-locator/index.html"
+  },
+  {
+    "revision": "1aa0de03eb81447355c2",
+    "url": "/ip-locator/static/css/main.7378c73f.chunk.css"
+  },
+  {
+    "revision": "5e12d8dea3a745dc53d5",
+    "url": "/ip-locator/static/js/2.81ebcd59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ip-locator/static/js/2.81ebcd59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa0de03eb81447355c2",
+    "url": "/ip-locator/static/js/main.5c1b1331.chunk.js"
+  },
+  {
+    "revision": "e93c3c83ea4c1dd49ba1",
+    "url": "/ip-locator/static/js/runtime-main.d9f495da.js"
+  }
+]);
